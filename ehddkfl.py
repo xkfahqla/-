d2bc0b926dbf5ffeef7064059c6a1a869c7ae9ad
https://github.com/xkfahqla <songhanju8972@naver.com>
2025-12-22 14:02:38 +0900

    sdhs

--- ehddkfl.py
+++ ehddkfl.py
@@ -11,38 +11,6 @@ player.collider = 'box'
 def input(key):
     if key=='escape':
         application.quit()
-    if key=="q":
-        drop_box=Entity(
-            model='cube',
-            scale=1,
-            position=(player.x,player.y-1,player.z),
-            color=color.red,
-            collider='box',
-        )
-        drop_box.gravity=10
-        try:
-            if drop_box.intersects(Floor).hit:
-                destroy(drop_box)
-        except Exception:
-            pass
-    if key=="e":
-        player.y-=1
-    if key=="q":
-        player.y+=1
-    if key=="r":
-        drop_box=Entity(
-            model='cube',
-            scale=1,
-            position=(player.x,player.y-1,player.z),
-            color=color.red,
-            collider='box'
-        )
-        drop_box.gravity=10
-        try:
-            if drop_box.intersects(Floor).hit:
-                destroy(drop_box)
-        except Exception:
-            pass
 Earth=[
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
@@ -73,22 +41,6 @@ for i in range(len(Earth)):
                 color=color.white,
                 collider='box'
             )
-        """
-        if A==2:
-            drop_box=Entity(
-                model='cube',
-                scale=1,
-                position=(i,100,j),
-                color=color.red,
-                collider='box',
-            )
-            drop_box.gravity=10
-            try:
-                if drop_box.intersects(Floor).hit:
-                    destroy(drop_box)
-            except Exception:
-                pass
-        """
         if A==3:
             Wall=Entity(
                 model='wireframe_cube',
